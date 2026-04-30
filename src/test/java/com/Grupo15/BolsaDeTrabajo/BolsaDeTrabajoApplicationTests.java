a812f1f6d38d5b26055106c8606d5c1170782124
FrancescoSantor <franchosantoro@hotmail.com>
2026-04-30 09:48:07 -0300

    Inicio

---
+++ src/test/java/com/Grupo15/BolsaDeTrabajo/BolsaDeTrabajoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Grupo15.BolsaDeTrabajo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolsaDeTrabajoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
