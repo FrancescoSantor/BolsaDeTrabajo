090f7fc066b920012fc33c88f761f8375558165a
EzeRas16 <ezeras2001@gmail.com>
2026-06-13 17:45:22 -0300

    MessageResponseDTO paso a ser de tipo record. El mapper de message ya no es con patron builder, ahora usa mapStruct. Refactor de message porque se declaraba como plural

--- src/main/java/com/Grupo15/BolsaDeTrabajo/Features/Message/Mapper/MessageMapper.java
+++ src/main/java/com/Grupo15/BolsaDeTrabajo/Features/Message/Mapper/MessageMapper.java
@@ -1,32 +1,14 @@
 package com.Grupo15.BolsaDeTrabajo.Features.Message.Mapper;
 
-import com.Grupo15.BolsaDeTrabajo.Features.Message.MessagesEntity;
-import com.Grupo15.BolsaDeTrabajo.Features.Message.dto.MessagesRequestDTO;
-import com.Grupo15.BolsaDeTrabajo.Features.Message.dto.MessagesResponseDTO;
+import com.Grupo15.BolsaDeTrabajo.Features.Message.MessageEntity;
+import com.Grupo15.BolsaDeTrabajo.Features.Message.dto.MessageRequestDTO;
+import com.Grupo15.BolsaDeTrabajo.Features.Message.dto.MessageResponseDTO;
+import org.mapstruct.Mapper;
 
-import java.sql.Timestamp;
-import java.time.Instant;
+@Mapper (componentModel = "spring")
+public interface MessageMapper {
 
-public class MessageMapper {
+    MessageEntity toEntity (MessageRequestDTO messageRequestDTO);
+    MessageResponseDTO toDto (MessageEntity messageEntity);
 
-    public static MessagesResponseDTO toDto(MessagesEntity entity)
-    {
-        return MessagesResponseDTO.builder()
-                .externalId(entity.getExternalId())
-                .issuerName(entity.getIssuer().getName())
-                .receptorName(entity.getReceptor().getName())
-                .content(entity.getContent())
-                .read(entity.isRead())
-                .createdAt(entity.getCreatedAt())
-                .build();
-    }
-
-    public static MessagesEntity toEntity(MessagesRequestDTO request)
-    {
-        return MessagesEntity.builder()
-                .content(request.content())
-                .read(false)
-                .createdAt(Timestamp.from(Instant.now()))
-                .build();
-    }
 }
