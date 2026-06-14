f93ae54dddcf33726f51957cb75a121221075979
FrancescoSantor <franchosantoro@hotmail.com>
2026-06-14 17:50:04 -0300

    Se Agregaron Los controllers y se agrego anotaciones en Repository

--- src/main/java/com/Grupo15/BolsaDeTrabajo/Features/LaboralExperience/ExperienceRepository.java
+++ src/main/java/com/Grupo15/BolsaDeTrabajo/Features/LaboralExperience/ExperienceRepository.java
@@ -1,11 +1,13 @@
 package com.Grupo15.BolsaDeTrabajo.Features.LaboralExperience;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface ExperienceRepository extends JpaRepository<LaboralExperienceEntity,Long> {
 
     // Busca una experiencia laboral específica usando el UUID seguro
