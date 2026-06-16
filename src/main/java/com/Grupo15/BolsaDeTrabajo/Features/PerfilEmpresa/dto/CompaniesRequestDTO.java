4f68072d9aa434dcc4613a8d42cc6cfbae005673
luca <alvarezlucagabriel2007@gmail.com>
2026-06-15 21:40:45 -0300

    agregado de validations a los dto de Company

--- src/main/java/com/Grupo15/BolsaDeTrabajo/Features/PerfilEmpresa/dto/CompaniesRequestDTO.java
+++ src/main/java/com/Grupo15/BolsaDeTrabajo/Features/PerfilEmpresa/dto/CompaniesRequestDTO.java
@@ -1,15 +1,28 @@
 package com.Grupo15.BolsaDeTrabajo.Features.PerfilEmpresa.dto;
 
 import com.Grupo15.BolsaDeTrabajo.Features.PerfilEmpresa.Category;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.UUID;
 
 public record CompaniesRequestDTO(
+        @NotBlank(message = "User name is required")
         String name,
+        @NotBlank(message = "Email is required")
+        @Email(message = "Email Format is not valid")
         String email,
+        @NotBlank(message = "cuit is required")
+        @Length(min = 9, message = "cuit must be at least 9 characters long")
         String cuit,
+        @NotNull(message = "category is required")
         Category category,
+        @NotBlank(message = "description was required")
         String description,
-        String location,
-        String webSite
+        @NotBlank(message = "Web site is required")
+        String webSite,
+        @NotBlank(message = "location is required")
+        String location
 ) {}
